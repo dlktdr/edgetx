da190258dfa9e5837ad1abdf892cfcce9b644fc5
dlktdr <cblackburn320@gmail.com>
2021-10-05 23:38:53 -0700

    Add a base serial class, generic enough that it can be used for AUX or USB
    My Idea is to create a SerialPort base class with a few virtual functions, Read/Write/Baud/Parity/Stops/DMA/Inverter. Which each driver will subclass USB or AUX.
    
    On top of this layer will be a serial_interface. Which is going to be next level of networking. e.g. (FRSky BT Module, RAW(passthrough), Wifi (wroom) module, HM10, etc...) Which will be responsible for configuring the device and sending data.
    
    On top of this layer will be a serial_function. Which is going to be Mavlink, Debug, Trainer, Telemetry, LUA.
    
    Which should allow you to send any data over any interface. I'm just not sure how strapped each target is on memory/flash if this will work overall.

--- radio/src/serial.h
+++ radio/src/serial.h
@@ -22,7 +22,65 @@
 #ifndef _SERIAL_H_
 #define _SERIAL_H_
 
+#include "targets/horus/board.h"
+
 #ifdef __cplusplus
+
+typedef enum {
+    FLOW_NONE = USART_HardwareFlowControl_None,
+    FLOW_RTS = USART_HardwareFlowControl_RTS,
+    FLOW_CTS = USART_HardwareFlowControl_RTS,
+    FLOW_RTSCTS = USART_HardwareFlowControl_RTS_CTS,
+} SerialFlow;
+
+typedef enum {
+    PARITY_NONE = USART_Parity_No,
+    PARITY_ODD = USART_Parity_Odd,
+    PARITY_EVEN = USART_Parity_Even,
+} SerialParity;
+
+typedef enum {
+    STOP_ONE=USART_StopBits_1,
+    STOP_TWO=USART_StopBits_2,
+    STOP_HALF=USART_StopBits_0_5,
+    STOP_ONE_AND_HALF= USART_StopBits_1_5,    
+} SerialStopBits;
+
+typedef enum {
+    DATA_8 = USART_WordLength_8b,
+    DATA_9 = USART_WordLength_9b
+} SerialDataBits;
+
+class SerialPort {
+public:
+    virtual int open()=0;
+    virtual int close()=0;
+    virtual void flush()=0;    
+    virtual void setDMAEnabled(bool d) {dma = d;}
+    void setDataBits(SerialDataBits data) {this->databits = data;}
+    void setBaudRate(uint32_t baud) {this->baud = baud;}
+    void setFlowControl(SerialFlow flow) {this->flow = flow;}
+    void setStopBits(SerialStopBits stopbits) {this->stopbits = stopbits;}
+    void setStopParity(SerialParity parity) {this->parity = parity;}
+    void setInvertedTX(bool inv) {invertedTX = inv;}
+    void setInvertedRX(bool inv) {invertedRX = inv;}
+    void setInverted(bool inv) {invertedRX = inv; invertedTX= inv;}
+    void setPowerOn(bool pwr) {poweron = pwr;}
+    virtual int putc(int ch)=0;
+    virtual int getc()=0;
+    
+protected:
+    uint32_t baud = 115200;
+    SerialDataBits databits = DATA_8;
+    SerialParity parity = PARITY_NONE;
+    SerialStopBits stopbits = STOP_ONE;
+    SerialFlow flow = FLOW_NONE;
+    bool invertedTX=false;
+    bool invertedRX=false;
+    bool dma=false;
+    bool poweron=false;
+};
+
 extern "C" {
 #endif
 
